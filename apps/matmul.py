5bf49890df585103d78cce64dd2b078f50a80cfc
Tony Wang <zihengw@stanford.edu>
2022-02-09 05:13:23 +0000

    (I believe) correct matmul implementation

--- apps/matmul.py
+++ apps/matmul.py
@@ -3,7 +3,7 @@ sys.path.append("/home/ubuntu/quokka/")
 
 import time
 from quokka_runtime import TaskGraph
-from sql import StatelessExecutor
+from sql import StatelessExecutor, AggExecutor
 import pandas as pd
 import numpy as np
 import ray
@@ -44,7 +44,7 @@ class MatMulExecutor(StatelessExecutor):
         if self.result is None:
             self.result = np.zeros((MATRIX_SIZE // BX, MATRIX_SIZE // BY))
 
-        batch = pd.concat(batches).numpy()[:,1:]
+        batch = pd.concat(batches).to_numpy()[:,1:]
         if stream_id == 0:
             
             row_start = sum([i.shape[0] for i in self.state0])
@@ -58,25 +58,30 @@ class MatMulExecutor(StatelessExecutor):
         
         if stream_id == 1:
             transposed = np.transpose(batch)
-            col_start = sum([i.shape[0] for i in self.state1])
-            col_end = col_start + batch.shape[0]
+            col_start = sum([i.shape[1] for i in self.state1])
+            col_end = col_start + transposed.shape[1]
+            print(col_start, col_end)
             row_start = 0
             for shard in self.state0:
                 row_end = row_start + shard.shape[0]
                 self.result[row_start: row_end, col_start : col_end] = np.dot(shard, transposed)
+                row_start = row_end
             self.state1.append(transposed)
-    
+   
     def done(self,executor_id):
         print(np.sum(self.result))
+        return pd.DataFrame([np.sum(self.result)])
         print("done join ", executor_id) 
 
 
-A = task_graph.new_input_csv("yugan","matrix.csv",["key"] + ["avalue" + str(i) for i in range(1000)],{'localhost':4})
-AT = task_graph.new_input_csv("yugan","matrix.csv",["key"] + ["avalue" + str(i) for i in range(1000)],{'localhost':4})
+A = task_graph.new_input_csv("yugan","matrix.csv",["key"] + ["avalue" + str(i) for i in range(MATRIX_SIZE)],{'localhost':4})
+AT = task_graph.new_input_csv("yugan","matrix.csv",["key"] + ["avalue" + str(i) for i in range(MATRIX_SIZE)],{'localhost':4})
 
 mm_executor = MatMulExecutor()
 #output_stream = task_graph.new_stateless_node({0:quotes,1:trades},join_executor,4,ip="172.31.48.233")
 output_stream = task_graph.new_stateless_node({0:A,1:AT},mm_executor,{'localhost':4},{0:partition_key_0, 1:partition_key_1})
+#agg_executor = AggExecutor()
+#agged = task_graph.new_stateless_node({0:output_stream}, agg_executor, {'localhost':1},{0:None})
 #counted = task_graph.new_stateless_node({0:output_stream}, count_executor, {'localhost':1}, {0:None})
 #output_executor = OutputCSVExecutor(4,"yugan","result")
 #wrote = task_graph.new_stateless_node({0:output_stream},output_executor,4)
