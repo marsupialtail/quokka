83a933db5bd5592c82cdb809025a53b1116fda9b
Ziheng Wang <ziheng@mit.edu>
2022-02-08 23:00:41 -0500

    matmul attempt

---
+++ apps/matmul.py
@@ -0,0 +1,89 @@
+import sys
+sys.path.append("/home/ubuntu/quokka/")
+
+import time
+from quokka_runtime import TaskGraph
+from sql import StatelessExecutor
+import pandas as pd
+import numpy as np
+import ray
+import os
+task_graph = TaskGraph()
+
+BX = 2
+BY = 2
+MATRIX_SIZE = 1000
+
+def partition_key_0(data, channel):
+    channel_x = channel // BY 
+    range_x_start = (MATRIX_SIZE // BX) * channel_x 
+
+    return data[ (data.key >= range_x_start ) & (data.key < range_x_start + (MATRIX_SIZE // BX)) ]
+
+def partition_key_1(data, channel):
+    channel_y = channel % BY 
+    range_y_start = (MATRIX_SIZE // BY) * channel_y
+
+    return data[ (data.key >= range_y_start) & (data.key < range_y_start + (MATRIX_SIZE//BY))]
+
+class MatMulExecutor(StatelessExecutor):
+    # this is basically an inner join, but we need to do some smart things to manage memory
+
+    def __init__(self):
+
+        
+        self.state0 = []
+        self.state1 = []
+        
+        # this is a Ray optimization. we should not instantiate the state when making the objects and passing them around
+        # we should insantiate them once they are on their machines. So instantiate state lazily
+        self.result = None 
+
+    def execute(self,batches, stream_id, executor_id):
+
+        if self.result is None:
+            self.result = np.zeros((MATRIX_SIZE // BX, MATRIX_SIZE // BY))
+
+        batch = pd.concat(batches).numpy()[:,1:]
+        if stream_id == 0:
+            
+            row_start = sum([i.shape[0] for i in self.state0])
+            row_end = row_start + batch.shape[0]
+            col_start = 0
+            for shard in self.state1:
+                col_end = col_start + shard.shape[1]
+                self.result[row_start:row_end, col_start : col_end] = np.dot(batch, shard)
+                col_start = col_end
+            self.state0.append(batch)
+        
+        if stream_id == 1:
+            transposed = np.transpose(batch)
+            col_start = sum([i.shape[0] for i in self.state1])
+            col_end = col_start + batch.shape[0]
+            row_start = 0
+            for shard in self.state0:
+                row_end = row_start + shard.shape[0]
+                self.result[row_start: row_end, col_start : col_end] = np.dot(shard, transposed)
+            self.state1.append(transposed)
+    
+    def done(self,executor_id):
+        print(np.sum(self.result))
+        print("done join ", executor_id) 
+
+
+A = task_graph.new_input_csv("yugan","matrix.csv",["key"] + ["avalue" + str(i) for i in range(1000)],{'localhost':4})
+AT = task_graph.new_input_csv("yugan","matrix.csv",["key"] + ["avalue" + str(i) for i in range(1000)],{'localhost':4})
+
+mm_executor = MatMulExecutor()
+#output_stream = task_graph.new_stateless_node({0:quotes,1:trades},join_executor,4,ip="172.31.48.233")
+output_stream = task_graph.new_stateless_node({0:A,1:AT},mm_executor,{'localhost':4},{0:partition_key_0, 1:partition_key_1})
+#counted = task_graph.new_stateless_node({0:output_stream}, count_executor, {'localhost':1}, {0:None})
+#output_executor = OutputCSVExecutor(4,"yugan","result")
+#wrote = task_graph.new_stateless_node({0:output_stream},output_executor,4)
+
+task_graph.initialize()
+
+start = time.time()
+task_graph.run()
+print("total time ", time.time() - start)
+#import pdb;pdb.set_trace()
