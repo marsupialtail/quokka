e418ed97543336ab993db74dca0790e0a489f06c
Ziheng Wang <zihengw@stanford.edu>
2023-04-14 15:38:20 -0700

    revert refactoring

--- pyquokka/executors.py
+++ pyquokka/executors.py
@@ -622,6 +622,60 @@ class SQLAggExecutor(Executor):
         del batch_arrow        
         return self.state
 
+class SortedAsofExecutor(Executor):
+    def __init__(self, time_col_trades = 'time', time_col_quotes = 'time', symbol_col_trades = 'symbol', symbol_col_quotes = 'symbol', suffix = "_right") -> None:
+        self.trade_state = None
+        self.quote_state = None
+        self.join_state = None
+        self.time_col_trades = time_col_trades
+        self.time_col_quotes = time_col_quotes
+        self.symbol_col_trades = symbol_col_trades
+        self.symbol_col_quotes = symbol_col_quotes
+        self.suffix = suffix
+
+    def execute(self,batches,stream_id, executor_id):    
+        # sort_col = self.time_col_trades if stream_id == 0 else self.time_col_quotes
+        # batch = polars.from_arrow(pa.concat_tables([batch.sort_by(sort_col) for batch in batches]))
+        batch = polars.from_arrow(pa.concat_tables(batches))
+        if stream_id == 0:
+            # assert batch[self.time_col_trades].is_sorted()
+            if self.trade_state is None:
+                self.trade_state = batch
+            else:
+                if len(self.trade_state) > 0:
+                    assert self.trade_state[self.time_col_trades][-1] <= batch[self.time_col_trades][0]
+                self.trade_state.vstack(batch, in_place = True)
+        else:
+            # assert batch[self.time_col_quotes].is_sorted()
+            if self.quote_state is None:
+                self.quote_state = batch
+            else:
+                if len(self.quote_state) > 0:
+                    assert self.quote_state[self.time_col_quotes][-1] <= batch[self.time_col_quotes][0]
+                self.quote_state.vstack(batch, in_place = True)
+
+        if self.trade_state is None or self.quote_state is None or len(self.trade_state) == 0 or len(self.quote_state) == 0:
+            return
+
+        joinable_trades = self.trade_state.filter(self.trade_state[self.time_col_trades] < self.quote_state[self.time_col_quotes][-1])
+        if len(joinable_trades) == 0:
+            return
+        
+        joinable_quotes = self.quote_state.filter(self.quote_state[self.time_col_quotes] <= joinable_trades[self.time_col_trades][-1])
+        if len(joinable_quotes) == 0:
+            return
+
+        self.trade_state =  self.trade_state.filter(self.trade_state[self.time_col_trades] >= self.quote_state[self.time_col_quotes][-1])
+        # self.quote_state = self.quote_state.filter(self.quote_state[self.time_col_quotes] >= joinable_quotes[self.time_col_quotes][-1])
+
+        result = joinable_trades.join_asof(joinable_quotes, left_on = self.time_col_trades, right_on = self.time_col_quotes, by_left = self.symbol_col_trades, by_right = self.symbol_col_quotes, suffix = self.suffix)
+        # print(len(result))
+
+        return result
+    
+    def done(self, executor_id):
+        return self.trade_state.join_asof(self.quote_state, left_on = self.time_col_trades, right_on = self.time_col_quotes, by_left = self.symbol_col_trades, by_right = self.symbol_col_quotes, suffix = self.suffix)
+
 class ConcatThenSQLExecutor(Executor):
     def __init__(self, sql_statement) -> None:
         self.statement = sql_statement
